1cd22e5aa43c04920448f23afee1dfceec734c41
Maksim Lukashevic <maksim.lukashevic@gmail.com>
2020-09-15 10:11:31 +0300

    duplicate deleted

--- pr/src/routeHistory.ts
+++
@@ -1,7 +0,0 @@
-import { createBrowserHistory } from 'history'
-
-const routeHistory = createBrowserHistory()
-
-export const goTo = (path: string): void => routeHistory.push(path)
-
-export default routeHistory
--- pr/src/store/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './models'
-
-export { default } from './store'
